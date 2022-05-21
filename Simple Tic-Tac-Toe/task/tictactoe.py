f4bf89ee32747d95e9eb9683c910377bfa662777
Cameron <c-em@outlook.com>
2022-05-20 17:35:58 -0700

    Added ability to add "X" to any open coordinate

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -5,16 +5,17 @@ from tictactoeutils import print_board, split_ttt_string
 tic_tac_toe_moves = input("Enter cells: ")
 game_grid = split_ttt_string(tic_tac_toe_moves)
 print_board(game_grid)
+while True:
+    coordinates = input("Enter the coordinates: ").split()
+    if not coordinates[0].isdigit() or not coordinates[1].isdigit() or len(coordinates) != 2:
+        print("You should enter numbers!")
+    elif int(coordinates[0]) not in range(1, 4) or int(coordinates[1]) not in range(1, 4):
+        print("Coordinates should be from 1 to 3!")
+    elif game_grid[int(coordinates[0]) - 1][int(coordinates[1]) - 1] == "X" \
+            or game_grid[int(coordinates[0]) - 1][int(coordinates[1]) - 1] == "O":
+        print("This cell is occupied! Choose another one!")
+    else:
+        game_grid[int(coordinates[0]) - 1][int(coordinates[1]) - 1] = "X"
+        break
 
-if abs(piece_count("X", game_grid) - piece_count("O", game_grid)) > 1 \
-        or (check_all(game_grid, "X") == 1 and check_all(game_grid, "O") == -1):
-    print("Impossible")
-elif check_all(game_grid, "X") == 1:
-    print("X wins")
-elif check_all(game_grid, "O") == -1:
-    print("O wins")
-elif piece_count("X", game_grid) + piece_count("O", game_grid) == 9:
-    print("Draw")
-else:
-    print("Game not finished")
-
+print_board(game_grid)
