f1f2baffa9d8e3ac49afa3be5f1d5fc38193868b
Cameron <c-em@outlook.com>
2022-05-20 17:54:12 -0700

    Added game loop.  Game fully functional

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,11 +1,11 @@
 from winchecks import check_all, piece_count
 from tictactoeutils import print_board, split_ttt_string
 
-
-tic_tac_toe_moves = input("Enter cells: ")
-game_grid = split_ttt_string(tic_tac_toe_moves)
+game_grid = split_ttt_string("         ")
 print_board(game_grid)
-while True:
+x_turn = True
+game_over = False
+while not game_over:
     coordinates = input("Enter the coordinates: ").split()
     if not coordinates[0].isdigit() or not coordinates[1].isdigit() or len(coordinates) != 2:
         print("You should enter numbers!")
@@ -15,7 +15,16 @@ while True:
             or game_grid[int(coordinates[0]) - 1][int(coordinates[1]) - 1] == "O":
         print("This cell is occupied! Choose another one!")
     else:
-        game_grid[int(coordinates[0]) - 1][int(coordinates[1]) - 1] = "X"
+        game_grid[int(coordinates[0]) - 1][int(coordinates[1]) - 1] = "X" if x_turn else "O"
+        x_turn = not x_turn
+    print_board(game_grid)
+    if check_all(game_grid, "X") == 1:
+        print("X wins")
+        break
+    elif check_all(game_grid, "O") == -1:
+        print("O wins")
+        break
+    elif piece_count("X", game_grid) + piece_count("O", game_grid) == 9:
+        print("Draw")
         break
 
-print_board(game_grid)
